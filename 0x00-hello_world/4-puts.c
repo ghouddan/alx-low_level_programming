f24911930e24499f69a9b8aa5b5219b9256e956f
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-10 03:23:17 -0700

    commiting changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 /**
- * the main function return the string needed
+ * main - Entry point of the program.
+ *
+ * This function demonstrates the usage of the puts function.
+ * It displays a message to the standard output.
+ *
+ * Return: Always returns 0 to indicate successful execution.
  */
 int main(void)
 {
