336c9e31fef8c03951445056f01a343a8c1a0ed0
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-10 04:14:58 -0700

    commiting changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,7 @@
  */
 int main(void)
 {
-const char *message = "and that piece of art is useful\"
-- Dora Korpar, 2015-10-19\n";
+char *message = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
 write (2, message, strlen(message));
 return(1);
 }
