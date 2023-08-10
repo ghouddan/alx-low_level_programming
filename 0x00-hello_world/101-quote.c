8aad10a807cff5a9f483683351a76f9e1c71b8bf
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-10 04:11:22 -0700

    commiting changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,16 @@
 #include <unistd.h>
 #include <string.h>
-
-int main(void) {
-    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, strlen(message));
-    return 1;
+/**
+ * main - Entry point of the program.
+ *
+ * This function demonstrates the usage of the puts function.
+ * It displays a message to the standard output.
+ *
+ * Return: Always returns 0 to indicate successful execution.
+ */
+int main(void)
+{
+const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, message, strlen(message));
+return(1);
 }
