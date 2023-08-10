dd4aec55b58f433e4cae35b302699efd8714b0fa
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-10 03:29:18 -0700

    commiting changes

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Entry point of the program.
+ *
+ * This function demonstrates the usage of the puts function.
+ * It displays a message to the standard output.
+ *
+ * Return: Always returns 0 to indicate successful execution.
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
