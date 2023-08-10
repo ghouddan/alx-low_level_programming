adbaea20407701157607204026d8e37bf59b72d6
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-10 02:59:08 -0700

    commiting changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
-int main(){
-    char *message ;
-    message="Programming is like building a multilingual puzzle";
-    puts(message);
-    return 0;
+int main(void)
+{
+char *message;
+message = "Programming is like building a multilingual puzzle";
+puts(message);
+return 0;
 }
