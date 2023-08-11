05447e63b88543a661de41b3c5b1b253b4b02c40
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-11 07:58:46 -0700

    if else tasck

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,13 +7,13 @@ n = rand();
 switch(n > 0)
 {
 case 1:
-printf("%d is positive.\n",n);
+printf("%d is positive\n",n);
 break;
 case 0:
 if (n == 0) {
-printf("%d is zero.\n",n);
+printf("%d is zero\n",n);
 } else {
-printf("%d is negative.\n",n);
+printf("%d is negative\n",n);
 }
 break;
 }
