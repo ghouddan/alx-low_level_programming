5a3645f742843d44e9077f1231336732e8a47a0e
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-11 10:22:19 -0700

    if else tasck

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,21 +1,26 @@
 #include <stdlib.h>
 #include <time.h>
 #include<stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point of the program.
+ *
+ * This function demonstrates the usage of the puts function.
+ * It displays a message to the standard output.
+ *
+ * Return: Always returns 0 to indicate successful execution.
+ */
 int main(void)
 {
 int n;
 int last;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-last = n%10;
+last = n % 10;
 if (last < 6 && last != 0)
-	printf("Last digit of %d is %d and is less than 6 and not 0\n",n,last);
-if(last > 5)
-	printf("last digit of %d is %d and is greater than 5\n",n,last);
-if(last == 0)
-	printf("last digit of %d id %d and is 0\n",n,last);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+if (last > 5)
+	printf("Last digit of %d is %d and is greater than 5\n", n, last);
+if (last == 0)
+	printf("Last digit of %d id %d and is 0\n", n, last);
 return (0);
 }
