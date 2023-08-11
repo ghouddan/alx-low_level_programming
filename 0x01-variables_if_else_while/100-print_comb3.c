4f9229270947b51dd07a7f483719d5e96956bdcf
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-11 14:06:05 -0700

    if else tasck

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,16 +8,17 @@
  * Return: Always returns 0 to indicate successful execution.
  */
 int main(void)
-{ 
+{
 int i;
 int j;
-for (i = 0; i < 10; i++){
+for (i = 0; i < 10; i++)
+{
 for (j = i+1; j < 10; j++)
 {
 if (i != j)
 {
-putchar('0'+i);
-putchar('0'+j);
+putchar('0' + i);
+putchar('0' + j);
 if (i != 8 || j != 9)
 {
 putchar(',');
