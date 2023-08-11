4bd13c8faa287fd1685be672b4fd0e8aeab54e49
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-11 14:19:33 -0700

    if else tasck

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ *
+ * This function demonstrates the usage of the puts function.
+ * It displays a message to the standard output.
+ *
+ * Return: Always returns 0 to indicate successful execution.
+ */
+int main(void)
+{
+int i,j,k;
+for (i = 0; i < 10; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+for (k = j + 1; k < 10; k++)
+{
+putchar('0' + i);
+putchar('0' + j);
+putchar('0' + k);
+if (i != 7 || j != 8 || k != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}            
+}
+}
+putchar('\n');
+return (0);
+}
