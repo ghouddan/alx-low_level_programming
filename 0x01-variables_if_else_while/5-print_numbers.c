2cb74f11b1d6c6446fde4dc0e0044a33901805f9
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-11 10:36:27 -0700

    if else tasck

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h> 
+/**
+ * main - Entry point of the program.
+ *
+ * This function demonstrates the usage of the puts function.
+ * It displays a message to the standard output.
+ *
+ * Return: Always returns 0 to indicate successful execution.
+ */
+int main(void)
+{
+int i;
+i=0;
+for(i = 0; i <=9; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+return (0);
+}
