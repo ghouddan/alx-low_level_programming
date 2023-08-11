0ca788c78378742c86898802be7595b71a27124a
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-11 08:14:30 -0700

    if else tasck

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entry point of the program.
+ *
+ * This function demonstrates the usage of the puts function.
+ * It displays a message to the standard output.
+ *
+ * Return: Always returns 0 to indicate successful execution.
+ */
+int main(void)
+{
+for(char c='a';c<='z';c++)
+{
+putchar(c);
+}
+printf("\n");
+return (0);
+}
+
