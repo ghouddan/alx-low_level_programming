d0ef22e6a6b43f47fc4b63df9e65fade75166606
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-11 07:31:04 -0700

    if else tasck

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+int n;
+n = rand();
+switch(n > 0)
+{
+case 1:
+printf("%d is positive.\n",n);
+break;
+case 0:
+if (n == 0) {
+printf("%d is zero.\n",n);
+} else {
+printf("%d is negative.\n",n);
+}
+break;
+}
+return (0);
+}
