faab5bc1c197c24b2e83f2306d9ae5dc433e3723
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-15 14:10:31 -0700

    function in c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,29 +5,35 @@
  * Return: On success, 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-void times_table() {
-int i;
-int j;
-int result;
-for (i = 0; i <= 9; i++) {
-for (j = 0; j <= 9; j++) {
-result = i * j;
-if (result >= 100) {
-_putchar(result / 100 + '0');
-result %= 100;
-}
-if (result >= 10) {
-_putchar(result / 10 + '0');
-}
-else if(j != 0)
+void times_table(void)
 {
-_putchar(' ');
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			k = j * i;
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
-_putchar(result % 10 + '0');
-if (j < 9) {
-_putchar(',');
-}
-}
-_putchar('\n');
-}
-}
+
