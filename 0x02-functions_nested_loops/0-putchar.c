39c85bddf5325e2906d03978a189f58ab2af39b4
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-15 06:46:55 -0700

    function in c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,6 @@
 #include<stdio.h>
 #include"main.h"
 /**
- * main - Entry point of the program.
- *
- * This function demonstrates the usage of the puts function.
- * It displays a message to the standard output.
- *
- * Return: Always returns 0 to indicate successful execution.
- */
-/**
  * function_name - Brief description of the function.
  * @parameter1: Description of parameter 1.
  * @parameter2: Description of parameter 2.
@@ -26,6 +18,14 @@ for (i = 0; i < 9; i++)
 putchar(put[i]);
 }
 }
+/**
+ * main - Entry point of the program.
+ *
+ * This function demonstrates the usage of the puts function.
+ * It displays a message to the standard output.
+ *
+ * Return: Always returns 0 to indicate successful execution.
+ */
 int main(void)
 {
 _putc();
