79e5ae3fd89426be9f1439a7cf76665fd789b999
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-15 10:29:02 -0700

    function in c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,6 @@
 #include"main.h"
 /**
- * _print_last_digit - Brief description of the function.
+ * print_last_digit - Brief description of the function.
  * @c : caracter to print
  * Detailed description of what the function does.
  * Return: Description of the return value.
@@ -9,6 +9,9 @@ int print_last_digit(int c)
 {
 int last;
 last = c % 10;
+if (last < 0)
+	last=-last;
 return (last);
 }
+
 
