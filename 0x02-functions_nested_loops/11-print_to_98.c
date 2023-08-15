b5e8c212732824211fc4a26495ac66520c1ddf88
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-15 14:39:51 -0700

    function in c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include"main.h"
 /**
  * print_to_98 - Brief description of the function.
- *
+ *@n: the input of the function
  * Detailed description of what the function does.
  * Return: Description of the return value.
  */
