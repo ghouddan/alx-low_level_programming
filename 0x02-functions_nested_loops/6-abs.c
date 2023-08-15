a0f3b3df22f5636f80425c233d7463f3ecae7acb
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-15 09:05:08 -0700

    function in c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include"main.h"
+/**
+ * _abs - Writes the alphabet in lowercase to stdout
+ * @c: The character to print
+ * Return: On success, 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _abs(int c)
+{
+if (c < 0)
+{
+return (-c);
+}
+if (c > 0)
+{
+return (c);
+}
+if (c == 0)
+{
+return (0);
+}
+}
