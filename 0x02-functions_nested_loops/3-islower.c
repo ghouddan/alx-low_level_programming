5baa7185484345a29ccdb3a3c9aa23505c7a0c37
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-15 08:55:59 -0700

    function in c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include"main.h"
 /**
  * _islower - Brief description of the function.
- *
+ * @c : caracter to print
  * Detailed description of what the function does.
  * Return: Description of the return value.
  */
