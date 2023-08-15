5eacde68b51f1146c8ac2dd1dce052955f568b2f
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-15 10:01:35 -0700

    function in c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,10 +5,8 @@
  * Return: On success, 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _abs(int c)
-{
-if (c < 0)
-{
+int _abs (int c){
+if (c < 0){
 return (-c);
 }
 if (c > 0)
@@ -19,5 +17,4 @@ if (c == 0)
 {
 return (0);
 }
-return (NULL);
 }
