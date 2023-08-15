584c6e674b034ef6ab1c880665799e00dd0a9c3b
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-15 08:46:28 -0700

    function in c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,6 @@
 #include"main.h"
 /**
  * _putc - writes the character c to stdout
- * @i: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
