8c566b02c2c647aa5ff55945e650639fc459c641
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-15 08:59:16 -0700

    function in c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _isalpha - Writes the alphabet in lowercase to stdout
- * c: The character to print
+ * @c: The character to print
  * Return: On success, 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
