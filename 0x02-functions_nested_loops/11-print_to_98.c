1e7678e28a6efb5d2e434ae12418d90abd9e9336
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-15 14:36:21 -0700

    function in c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,33 +1,34 @@
-#include"main.h"
 #include<stdio.h>
+#include"main.h"
 /**
- * print_to_98 - Writes the alphabet in lowercase to stdout
- * @c: The character to print
- * Return: On success, 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * print_to_98 - Brief description of the function.
+ *
+ * Detailed description of what the function does.
+ * Return: Description of the return value.
  */
+
 void print_to_98(int n)
 {
-int i;
-int j;
-if (n == 98)
-	printf("%d",n);
-if (n < 98)
-{
-for (i = n; i <= 98; i++)
-{
-	printf("%d",i);
-	putchar(',');
-	putchar(' ');
+	int i, j;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	} else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
+		}
+	}
 }
-}
-else
-{
-for(j = n; j < 99; j--)
-{
-	printf("%d",j);
-	putchar(',');
-        putchar(' ');
-}
-}
-}
+
