fd6c833fbab43fed510529c469c1c3bcd4634249
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-15 10:59:34 -0700

    function in c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,13 +1,26 @@
 #include "main.h"
+/**
+ * print_sign - Brief description of the function.
+ * @n : caracter to print
+ * Detailed description of what the function does.
+ * Return: Description of the return value.
+ */
 int print_sign(int n)
 {
 if (n < 0)
+{
 _putchar('-');
 return (-1);
+}
 if (n > 0)
+{
 _putchar('+');
 return (1);
+}
+{
 if (n == 0)
 _putchar('0');
-return (0);    	
+return (0);
+}
+return (0);
 }
