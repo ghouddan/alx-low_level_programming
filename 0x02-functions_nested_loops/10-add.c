5b713b2813e59a4d96fe45654dc3977169e1b511
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-15 14:14:56 -0700

    function in c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+/**
+ * add - Writes the alphabet in lowercase to stdout
+ * c: The character to print
+ * Return: On success, 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int add(int a,int b)
+{
+int c;
+c = a + b;
+return (c);
+}
