89c97a122a2b8a88067d55d3ecc800df4f51f686
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-15 12:05:32 -0700

    function in c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include"main.h"
+/**
+ * time_table - Writes the alphabet in lowercase to stdout
+ *  The character to print
+ * Return: On success, 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void time_table(void)
+{
+for (int i = 0; i <= 9; i++)
+{
+for (int j = 0; j <= 9; j++)
+{
+int result = i * j;
+_putchar(result / 10 + '0');  
+_putchar(result % 10 + '0');  
+if (j < 9)
+{
+_putchar(',');  // Print comma
+_putchar(' ');  // Print space
+}
+}
+putchar('\n');  // Print newline
+}
+}
