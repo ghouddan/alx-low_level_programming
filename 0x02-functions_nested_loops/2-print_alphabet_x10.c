d44d7fab28db6a37acac086120968837dd5681f3
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-15 08:52:59 -0700

    function in c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include "main.h"
 /**
- * print_alphabet - Writes the alphabet in lowercase to stdout
+ * print_alphabet_x10 - Writes the alphabet in lowercase to stdout
  * c: The character to print
  * Return: On success, 1.
  * On error, -1 is returned, and errno is set appropriately.
