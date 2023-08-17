64ddd9ca03aebdc24a4ebf82ec40e12e5c7c02e2
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-17 04:07:48 -0700

    commit changes

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * isupper - Brief description of the function.
+ *@c: variable to check 
+ * Detailed description of what the function does.
+ * Return: Description of the return value.
+ */
+int _isupper(int c)
+{
+if(c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this folder is related to the c function 
