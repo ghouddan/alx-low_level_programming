e751a79545164e568d5c9cae8927a05668f85f4a
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-17 04:08:55 -0700

    commit changes

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * isupper - Brief description of the function.
- *@c: variable to check 
+ * _isupper - Brief description of the function.
+ * @c: variable to check 
  * Detailed description of what the function does.
  * Return: Description of the return value.
  */
