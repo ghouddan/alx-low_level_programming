e3c6cfd688a9c9f987d907e74c413445d39f91fd
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-17 02:51:37 -0700

    commit changes

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,13 @@
 #include "main.h"
 #include<stdio.h>
 /**
-* main - Test function for positive or negative
-* Return: 0
-*/
+ * main - Entry point of the program.
+ *
+ * This function demonstrates the usage of the puts function.
+ * It displays a message to the standard output.
+ *
+ * Return: Always returns 0 to indicate successful execution.
+ */
 int main(void)
 {
 int i;
