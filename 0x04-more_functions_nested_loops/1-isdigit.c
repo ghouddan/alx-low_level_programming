6a00cc0325c1c6e4c8e6f61becc5a5fa8a9b8d38
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-17 07:05:07 -0700

    commit changes

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * _isdigit - writes the character c to stdout
- * @c : the variable to check
- * Return: On success 1.
+ * _isdigit - Brief description of the function.
+ * @c : caracter to print
+ * Detailed description of what the function does.
+ * Return: Description of the return value.
  */
 int _isdigit(int c)
 {
-if(c >= 48 && c <= 57)
+if (c >= 48 && c <= 57)
 {
 return (1);
 }
