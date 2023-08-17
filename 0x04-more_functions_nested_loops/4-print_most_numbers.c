aeb94dc56e9ed8f6fc68a7bcba9b9913d3421425
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-17 11:35:11 -0700

    commit changes

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_last_digit - Brief description of the function.
+ * print_most_numbers - Brief description of the function.
  * @i : caracter to print
  * Detailed description of what the function does.
  * Return: Description of the return value.
@@ -8,7 +8,7 @@
 void print_most_numbers(void)
 {
 int i;
-for (i =0; i < 10; i++)
+for (i = 0; i < 10; i++)
 {
 if (i != 2 && i != 4)
 {
