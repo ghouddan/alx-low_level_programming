b2306c1427dc67c3895d0fd1935676d5a30e884b
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-17 12:32:57 -0700

    commit changes

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"/**
+ * print_line - Brief description of the function.
+ * @n : number of time
+ * Return: Description of the return value.
+ */
+ void print_line(int n)
+{
+int i;
+if (n > 0)
+{
+for(i = 1; i <= n; i++)
+{
+_putchar(95);
+}
+_putchar('\n');
+}
+else
+{
+_putchar('\n');
+}
+}
