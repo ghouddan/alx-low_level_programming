cfe525977f055fd3b7fc0800437a820c28b53e09
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-17 12:24:16 -0700

    commit changes

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,18 +1,17 @@
 #include "main.h"
 /**
  * more_numbers - Brief description of the function.
- * Detailed description of what the function does.
  * Return: Description of the return value.
  */
 void more_numbers(void)
 {
 int i;
 int j;
-for ( j = 0; j < 10; j++)
+for (j = 0; j < 10; j++)
 {
 for (i = 0; i < 15; i++)
 {
-if (i > 10)
+if (i >= 10)
 {
 _putchar('1');
 }
