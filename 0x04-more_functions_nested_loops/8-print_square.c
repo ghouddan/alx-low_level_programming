dff31a0a5c17e9a3becb5437b60f96062e4be788
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-17 13:20:25 -0700

    commit changes

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_line - Brief description of the function.
+ * @n : size of the square
+ */
+void print_square(int n)
+{
+int i;
+int j;
+if (n > 0)
+{
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j < n; j++)
+{
+if(i != j)
+{
+_putchar('#');
+}
+else{
+_putchar('\n'); 
+}
+}
+}
+}
+}
