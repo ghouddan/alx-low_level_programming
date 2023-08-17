13eb730572702c01d675527bedbaf9d52007ae33
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-17 06:41:10 -0700

    commit changes

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,14 +3,13 @@
  * print_numbers - writes the character c to stdout
  * @i: number to print
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
  */
 void print_numbers(void)
 {
 int i;
 for (i = 0; i < 10; i++)
 {
-_putchar(i+'0');
+_putchar(i + '0');
 }
 _putchar('\n');
 }
