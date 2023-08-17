2c0d7861ff41126484ca656dcd1745034e20c515
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-17 12:46:30 -0700

    commit changes

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+include "main.h"
+/**
+ * print_diagonal - Brief description of the function.
+ * @n : number of time
+ */
+void print_line(int n)
+{
+int i;
+if (n > 0)
+{
+for (i = 1; i <= n; i++)
+{
+_putchar('\');
+}
+_putchar('\n');
+}
+else
+{
+_putchar('\n');
+}
+}
