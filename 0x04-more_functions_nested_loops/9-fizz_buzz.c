22082afd63e5e15a5d6d5166f91857e01a426adc
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-17 16:08:54 -0700

    commit changes

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,11 +1,8 @@
-#include"main.h"
+#include "main.h"
 #include<stdio.h>
 /**
  * main - Entry point of the program.
- *
  * This function demonstrates the usage of the puts function.
- * It displays a message to the standard output.
- *
  * Return: Always returns 0 to indicate successful execution.
  */
 int main(void)
