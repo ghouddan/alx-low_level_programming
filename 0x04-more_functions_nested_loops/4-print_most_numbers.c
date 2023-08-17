05b10a824fa5b33692889ec61bf613e6c7397236
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-17 06:40:09 -0700

    commit changes

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_last_digit - Brief description of the function.
+ * @i : caracter to print
+ * Detailed description of what the function does.
+ * Return: Description of the return value.
+ */
+void print_most_numbers(void)
+{
+int i;
+for (i =0; i < 10; i++)
+{
+if (i != 2 && i != 4)
+{
+_putchar(i + '0');
+}
+}
+_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,4 +5,5 @@ int _putchar(char);
 int _isdigit(int);
 int mul(int, int);
 void print_numbers(void);
+void print_most_numbers(void);
 #endif
