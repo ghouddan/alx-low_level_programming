0a7d2f3912a5a5af35bc5049f8acc81c7cde2b79
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-17 14:01:58 -0700

    commit changes

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,43 +12,6 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if ( i % 5 == 0 && i % 3 == 0)
-{
-printf(" FizzBuzz");
-}
-else if (i % 5 == 0)
-{
-printf(" Buzz");
-}
-else if (i % 3 == 0)
-{
-printf(" Fizz");
-}
-else if (i == 1)
-{
-printf(" %d",i);
-}
-else
-{
-printf(" %d",i);
-}
-}
-printf("\n");
-return (0);
-}#include<stdio.h>
-/**
- * main - Entry point of the program.
- *
- * This function demonstrates the usage of the puts function.
- * It displays a message to the standard output.
- *
- * Return: Always returns 0 to indicate successful execution.
- */
-int main(void)
-{
-int i;
-for (i = 1; i <= 100; i++)
-{
 if (i % 5 == 0 && i % 3 == 0)
 {
 printf(" FizzBuzz");
