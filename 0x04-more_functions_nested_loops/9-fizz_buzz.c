6ea67d5977e7b4d823717f50dbd9c44cfc4d4f6f
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-17 16:05:01 -0700

    commit changes

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,5 @@
-#include "main.h"
-#include <stdio.h>
-
+#include"main.h"
+#include<stdio.h>
 /**
  * main - Entry point of the program.
  *
@@ -11,33 +10,30 @@
  */
 int main(void)
 {
-    int i;
-
-    for (i = 1; i <= 100; i++)
-    {
-        if ((i % 5 == 0) && (i % 3 == 0))
-        {
-            printf(" FizzBuzz");
-        }
-        else if (i % 5 == 0)
-        {
-            printf(" Buzz");
-        }
-        else if (i % 3 == 0)
-        {
-            printf(" Fizz");
-        }
-        else if (i == 1)
-        {
-            printf("%d", i);
-        }
-        else
-        {
-            printf(" %d", i);
-        }
-    }
-
-    printf("\n");
-    return (0);
+int i;
+for (i = 1; i <= 100; i++)
+{
+if ((i % 5 == 0) && (i % 3 == 0))
+{
+printf(" FizzBuzz");
 }
-
+else if (i % 5 == 0)
+{
+printf(" Buzz");
+}
+else if (i % 3 == 0)
+{
+printf(" Fizz");
+}
+else if (i == 1)
+{
+printf("%d",i);
+}
+else
+{
+printf(" %d",i);
+}
+}
+printf("\n");
+return (0);
+}
