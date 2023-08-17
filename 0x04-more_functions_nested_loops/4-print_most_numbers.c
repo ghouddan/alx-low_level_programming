1c4a445a21e6ec05e8a30e46611f4096acb16d92
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-17 11:36:19 -0700

    commit changes

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * print_most_numbers - Brief description of the function.
- * @i : caracter to print
  * Detailed description of what the function does.
  * Return: Description of the return value.
  */
