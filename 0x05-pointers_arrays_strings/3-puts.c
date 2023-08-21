fe38efdec49f0975eee692e0d5e152a6725a6269
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-21 09:42:04 -0700

    commit chnages

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _put - Brief description of the function.
+ * @c : caracter to print
+ * Detailed description of what the function does.
+ * Return: Description of the return value.
+ */
+void _puts(char *s){
+int i;
+i = 0;
+while (s[i]){
+_putchar(s[i]);
+i++;
+}
+_putchar('\n');
+return (i);
+}
