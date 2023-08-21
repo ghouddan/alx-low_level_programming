55ffdbdb972b67ced9ebefbbabfee8b89ff266bd
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-21 12:14:52 -0700

    commit chnages

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _print_array - Brief description of the function.
+ * @n : array size
+ * @a : caracter to print
+ * Detailed description of what the function does.
+ * Return: Description of the return value.
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+if (i == n - 1)
+{
+printf("%d",a[i]);
+}
+else
+{
+printf("%d, ",a[i]);
+}
+}
+}
