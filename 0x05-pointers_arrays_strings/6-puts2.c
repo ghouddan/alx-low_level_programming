69737c8cadbb2d3c89b27866a35850230f942fbd
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-21 14:53:31 -0700

    commit chnages

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,7 +5,7 @@
  * Detailed description of what the function does.
  * Return: Description of the return value.
  */
-void puts2(char *str)
+/*void puts2(char *str)
 {
 int i;
 i = 0;
@@ -15,5 +15,22 @@ _putchar(str[i]);
 i = i + 2;
 }
 _putchar('\n');
+}*/
+void puts2(char *str)
+{
+int size;
+int i;
+size = 0;
+while (str[size] != '\0')
+{
+size++;
 }
+for (i = 0; i < size; i++){
+if (i % 2 == 0){
+putchar(str[i]);
+}
+}
+putchar('\n');
+}
+
 
