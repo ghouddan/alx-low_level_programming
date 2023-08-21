0ebd087d7fd9b02b6c1e053b9902aa21c62278ac
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-21 14:31:21 -0700

    commit chnages

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * puts_half - Brief description of the function.
+ * puts2 - Brief description of the function.
  * @str : caracter to print
  * Detailed description of what the function does.
  * Return: Description of the return value.
  */
-void puts_half(char *str)
+void puts2(char *str)
 {
 int i;
 i = 0;
