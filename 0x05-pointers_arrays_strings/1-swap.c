e57ee3410bc8f7ad3730f5708c7c9fad6648ddb5
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-21 07:56:00 -0700

    commit chnages

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"/**
+ * swap_int - Brief description of the function.
+ * @b : variable 2
+ * @a : variable 1
+ * Detailed description of what the function does.
+ * Return: Description of the return value.
+ */
+ void swap_int(int *a, int *b)
+{
+int *tmp;
+*tmp = *a;
+*a = *b;
+*b = tmp;
+}
