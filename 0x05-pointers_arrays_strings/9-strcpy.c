e117ee2ed71438e9da075d70d89012cae2b51c6d
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-21 12:54:32 -0700

    commit chnages

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,7 +2,7 @@
 /**
  * _strcpy - Brief description of the function.
  * @src : source
- * @dest : destination 
+ * @dest : destination
  * Detailed description of what the function does.
  * Return: Description of the return value.
  */
