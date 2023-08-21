5891d3cd417925c2d9f83e22edd84ce69577f70d
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-21 15:20:26 -0700

    commit chnages

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,7 +5,7 @@
  * Detailed description of what the function does.
  * Return: Description of the return value.
  */
-/*void puts_half(char *str)
+void puts_half(char *str)
 {
 int size;
 int i;
@@ -23,14 +23,14 @@ _putchar(str[i]);
 }
 else
 {
-for (i = (size - 1) / 2; i < size ; i++)
+for (i = (size + 1) / 2; i < size ; i++)
 {
 _putchar(str[i]);
 }
 }
 _putchar('\n');
-}*/
-void puts_half(char *str)
+}
+/*void puts_half(char *str)
 {
 	int a, n, longi;
 
@@ -48,4 +48,4 @@ void puts_half(char *str)
 		_putchar(str[a]);
 	_putchar('\n');
 }
-
+*/
