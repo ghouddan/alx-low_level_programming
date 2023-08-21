51792363bbaf1de87d47e76c99438c5ddd098d4d
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-21 09:34:14 -0700

    commit chnages

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * strlen - Brief description of the function.
+ * @s : caracter to print
+ * Detailed description of what the function does.
+ * Return: Description of the return value.
+ */
+int _strlen(char *s){
+int i;
+i=0;
+while(s[i]){
+i++;
+}
+return (i);
+}
