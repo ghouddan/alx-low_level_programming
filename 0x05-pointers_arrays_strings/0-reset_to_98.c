78a4832460e67db5c01e05849c0197b671981e5c
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-21 06:16:52 -0700

    commit chnages

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * reset_to_98 - Brief description of the function.
- * @*n : caracter to print
+ * @n : caracter to print
  * Detailed description of what the function does.
  * Return: Description of the return value.
  */
