548a8c155f3a0f6b1b613877cea6bc4078102e8b
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-23 13:01:51 -0700

    commit changes

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_array -  Brief description of the function.
+ * @s : caracter to print
+ * Detailed description of what the function does.
+ * Return: Description of the return value.
+ */
+void reverse_array(int *a, int n)
+{
+    int i;
+    int tmp;
+    int j;
+    for(i = 0; i< n; i++, n--)
+    {
+        tmp = a[i];
+        a[i] = a[n- 1] ;
+        a[n - 1] = tmp;
+    }
+}
+
+
