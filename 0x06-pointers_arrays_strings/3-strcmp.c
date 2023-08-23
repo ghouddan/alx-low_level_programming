69874d83ec1310ed9e5ea40e6bc028f145fcf943
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-23 12:16:42 -0700

    commit changes

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _strcmp - Brief description of the function.
+ * @s1 : string 1
+ * @s2 : string 2
+ * @n : number of byte form src
+ * Detailed description of what the function does.
+ * Return: Description of the return value.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int i;
+int result;
+i = 0;
+while ((s1[i] != '\0') && (s2[i] != '\0'))
+{
+if (s1[i] > s2[i])
+{
+result = s1[i] - s2[i]; 
+return (result);
+}
+else if (s1[i] < s2[i])
+{
+result = s1[i] - s2[i];
+return (result);
+}
+else
+{
+    return (0);
+}
+i++;
+}
+return (0);
+}
