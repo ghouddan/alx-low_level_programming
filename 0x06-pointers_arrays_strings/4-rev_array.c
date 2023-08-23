b756315eca5d5f10ff25ac3830860a35658fdd56
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-23 14:49:47 -0700

    commit changes

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,14 +8,14 @@
  */
 void reverse_array(int *a, int n)
 {
-    int i;
-    int tmp;
-    for(i = 0; i< n; i++, n--)
-    {
-        tmp = a[i];
-        a[i] = a[n- 1] ;
-        a[n - 1] = tmp;
-    }
+int i;
+int tmp;
+for (i = 0; i< n; i++, n--)
+{
+tmp = a[i];
+a[i] = a[n- 1] ;
+a[n - 1] = tmp;
+}
 }
 
 
