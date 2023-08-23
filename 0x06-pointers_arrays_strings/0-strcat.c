33f0dd9703067d7802d62183c03c13c15a871e04
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-23 11:05:37 -0700

    commit changes

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strcat- Brief description of the function.
+ * _strcat - Brief description of the function.
  * @src : string to concatanat
  * @dest : caracter to print
  * Detailed description of what the function does.
@@ -8,7 +8,7 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int size1,size2,i;
+int size1, size2, i;
 size2 = 0;
 size1 = 0;
 while (dest[size1])
