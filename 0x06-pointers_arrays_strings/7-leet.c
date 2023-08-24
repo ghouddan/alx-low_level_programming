355d0f477868c0689f942bd94ca6cf4d7caf1920
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-24 14:54:27 -0700

    commit changes

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * leet -  Brief description of the function.
+ * leet - Brief description of the function.
  * @str : string to change
  * Detailed description of what the function does.
  * Return: Description of the return value.
