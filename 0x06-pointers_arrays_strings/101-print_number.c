410144e1b7bdf1637c860b72a72d021c45e928fb
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-24 14:53:07 -0700

    commit changes

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
- * reverse_array -  Brief description of the function.
- * @a : array to print
- * @n : size of array
+ * print_number -  Brief description of the function.
+ * @n : number to print
  * Detailed description of what the function does.
  * Return: Description of the return value.
  */
