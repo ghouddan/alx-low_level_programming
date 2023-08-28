d34e8c2ea4265a8fb00c86f6c896e5f62cf8102c
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-28 06:45:52 -0700

    commit changes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _strpbrk - Brief description of the function.
- * @dest : array to print
+ * @s : array to print
  * @accept : source array
  * Detailed description of what the function does.
  * Return: Description of the return value.
