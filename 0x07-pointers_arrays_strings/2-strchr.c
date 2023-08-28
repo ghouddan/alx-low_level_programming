535daa56d369fd0fa5ef03600fd2202ac70e7cf7
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-28 11:55:53 -0700

    commit changes

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _strchr - Brief description of the function.
- * @s: string 
+ * @s: string
  * @c : character to find
  * Detailed description of what the function does.
  * Return: Description of the return value.
