791ada035dada3ef894803a5d046e483275b92a1
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-28 11:40:51 -0700

    commit changes

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _set_string - Brief description of the function.
+ * @s: string
+ * @to : character affect
+ * Detailed description of what the function does.
+ * Return: Description of the return value.
+ */
+void set_string(char **s, char *to)
+{
+    *s=to;
+}
