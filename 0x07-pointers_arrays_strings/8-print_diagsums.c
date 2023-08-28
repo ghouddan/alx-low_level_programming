d5f530988f4db9eb28cde9b95ec90af0fd3640c3
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-28 11:37:14 -0700

    commit changes

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_diagsums - Brief description of the function.
+ * @a : matrix
+ * @size : size of the matrix
+ * Detailed description of what the function does.
+ * Return: Description of the return value.
+ */
+void print_diagsums(int *a, int size)
+{
+int sum2;
+int i;
+int sum1;
+sum1 = 0;
+sum2 = 0;
+for (i = 0; i < size; i++)
+{
+sum1 += a[i * size + i]; 
+sum2 += a[i * size + (size - 1 - i)];
+}
+printf("%d, %d\n", sum1,sum2);
+}
