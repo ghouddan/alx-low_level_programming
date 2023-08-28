dacf142676d48523cf6aacf0f6ce1427ebf8244c
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-28 10:07:33 -0700

    commit changes

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_chessboard- Brief description of the function.
+ * @a : chessboar
+ * Detailed description of what the function does.
+ * Return: Description of the return value.
+ */
+void print_chessboard(char (*a)[8])
+{
+int j;
+int i;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+_putchar(a[i][j]);
+ 
+}
+_putchar('\n');
+}
+}
