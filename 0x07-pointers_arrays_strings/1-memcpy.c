368f7a2a35066b877c9a61e0fbbd43b7e393cbea
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-28 11:51:17 -0700

    commit changes

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,11 +7,12 @@
  * Detailed description of what the function does.
  * Return: Description of the return value.
  */
-char *_memcpy(char *dest, char *src, unsigned int n){
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
 unsigned int i;
 i = 0;
 while (i < n)
-{ 
+{
 dest[i] = src[i];
 i++;
 }
