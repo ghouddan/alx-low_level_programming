7d62ad87cbfd0d240f2f96864aefa6781d247399
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-30 13:34:30 -0700

    commit changes

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * is_prime_number - Brief description of the function.
+ * @n: number to determine
+ * @i : number to check for prime
+ * Detailed description of what the function does.
+ * Return: Description of the return value.
+ */
+int prime(int n, int i);
+int is_prime_number(int n)
+{
+if (prime(n, 2) == n)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
+/**
+ * prime - Brief description of the function.
+ * @n: number to determine
+ * @i : number to check for prime
+ * Detailed description of what the function does.
+ * Return: Description of the return value.
+ */
+int prime(int n, int i)
+{
+if (n <= 1)
+{
+return (0);
+}
+if(i * i > n)
+{
+return (n);
+}
+if (n % i == 0) {
+return i;
+}
+return (prime(n, i + 1));
+}
