ccf9ddfc55331ed286b09a3842a50c5057377b25
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-30 12:45:02 -0700

    commit changes

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,17 +2,22 @@
  /**
  * _sqrt_recursion - Brief description of the function.
  * @n : number to find the sqr of
- * @i : to check for the sqr
  * Detailed description of what the function does.
  * Return: Description of the return value.
  */
-int sqr(int n, int i);
 int _sqrt_recursion(int n)
 {
 if (n < 0)
 return (-1);
 return (sqr(n, 0)); 
 }
+ /**
+ * sqr - Brief description of the function.
+ * @n : number to find the sqr of
+ * @i : to check for the sqr
+ * Detailed description of what the function does.
+ * Return: Description of the return value.
+ */
 int sqr(int n, int i)
 {
 if (i * i > n)
