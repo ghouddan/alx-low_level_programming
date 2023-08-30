b50806ab38bcc034180fb2c172ff8a9c128ac362
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-30 07:12:20 -0700

    commit changes

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+include "main.h"
+/**
+ * _print_rev_recursion - Brief description of the function.
+ * @s : string to print
+ * Detailed description of what the function does.
+ * Return: Description of the return value.
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+{
+return ;
+}
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
