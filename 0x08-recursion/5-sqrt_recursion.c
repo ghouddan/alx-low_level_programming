dab00e6fd97355d631517fa504f2eb99c5e1bab0
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-30 13:01:00 -0700

    commit changes

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,6 +2,7 @@
 /**
  * _sqrt_recursion - Brief description of the function.
  * @n : number to find the sqr of
+ * @i : to check for the root
  * Detailed description of what the function does.
  * Return: Description of the return value.
  */
