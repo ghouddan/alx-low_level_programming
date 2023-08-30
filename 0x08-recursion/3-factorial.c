1bfab0dbc363a3ee95738eafe0cb20e3a2debd97
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-08-30 12:00:25 -0700

    commit changes

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * factorial - Brief description of the function.
+ * @n : numbers 
+ * Detailed description of what the function does.
+ * Return: Description of the return value.
+ */
+int factorial(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+if (n == 0)
+{
+return (1);
+}
+return (n *= factorial(n-1));
+}
+
