a87d4cdc7362b326efea81397ee171b1a9cabd45
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-01 05:19:28 -0700

    commit changes

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ * This function demonstrates the usage of the puts function.
+ * It displays a message to the standard output.
+ * Return: Always returns 0 to indicate successful execution.
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n",argv[0]);
+return (0);
+}
