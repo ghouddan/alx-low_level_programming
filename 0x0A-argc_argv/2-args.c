f7fb2b2d748ed8c96336e09bd42babf282eff7b0
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-01 06:01:04 -0700

    commit changes

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program.
+ * @argc : argument count
+ * @argv : argument vector
+ * This function demonstrates the usage of the puts function.
+ * It displays a message to the standard output.
+ * Return: Always returns 0 to indicate successful execution.
+ */
+int main(int argc, char *argv[])
+{
+int i;
+for(i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
