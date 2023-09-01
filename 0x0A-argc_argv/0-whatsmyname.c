c9b71c0d183eddfe30cc6ca5cf075d983cca7e67
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-01 05:29:44 -0700

    commit changes

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
  * main - Entry point of the program.
+ * @argc : argument count
+ * @argv : argument vector
  * This function demonstrates the usage of the puts function.
  * It displays a message to the standard output.
  * Return: Always returns 0 to indicate successful execution.
@@ -9,7 +11,7 @@ int main(int argc, char *argv[])
 {
 if (argc > 0)
 {
-printf("%s\n",argv[0]);
+printf("%s\n", argv[0]);
 }
 else
 {
