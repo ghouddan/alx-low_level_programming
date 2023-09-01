6c99578a91da0e98707e427a99dfb9ddb209c545
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-01 06:56:33 -0700

    commit changes

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * main - Entry point of the program.
+ * @argc : argument count
+ * @argv : argument vector
+ * This function demonstrates the usage of the puts function.
+ * It displays a message to the standard output.
+ * Return: Always returns 0 to indicate successful execution.
+ */
+int main(int argc, char *argv[]) {
+int i;
+int j;
+int sum;
+sum = 0;
+if (argc == 1)
+{
+printf("0\n");
+return 0;
+}
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (!isdigit(argv[i][j]))
+{
+printf("Error\n");
+return 1;
+}
+}
+sum += atoi(argv[i]);
+}
+printf("%d\n", sum);
+return 0;
+}
