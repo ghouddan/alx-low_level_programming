b9bd4fd04e0bf10a8d7520363176a2108a92433f
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-01 06:57:57 -0700

    commit changes

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,7 +9,8 @@
  * It displays a message to the standard output.
  * Return: Always returns 0 to indicate successful execution.
  */
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 int i;
 int j;
 int sum;
@@ -17,7 +18,7 @@ sum = 0;
 if (argc == 1)
 {
 printf("0\n");
-return 0;
+return (0);
 }
 for (i = 1; i < argc; i++)
 {
@@ -26,11 +27,11 @@ for (j = 0; argv[i][j] != '\0'; j++)
 if (!isdigit(argv[i][j]))
 {
 printf("Error\n");
-return 1;
+return (1);
 }
 }
 sum += atoi(argv[i]);
 }
 printf("%d\n", sum);
-return 0;
+return (0);
 }
