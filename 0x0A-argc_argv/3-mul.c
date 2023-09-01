0cd9829d43e06c005c515cd7ff8d54b123c78478
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-01 06:21:08 -0700

    commit changes

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,14 +10,14 @@
  */
 int main(int argc, char *argv[])
 {
-if(argc == 3)
+if (argc == 3)
 {
 printf("%d\n",atoi(argv[1]) * atoi(argv[2]));
 return (0);
 }
 else
 {
-printf("error\n");
+printf("Error\n");
 return (1);
 }
 }
