2d832e1a6e6c2e748564c8bbc74b8a1cd2d76f48
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-01 06:19:56 -0700

    commit changes

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point of the program.
+ * @argc : argument count
+ * @argv : argument vector
+ * This function demonstrates the usage of the puts function.
+ * It displays a message to the standard output.
+ * Return: Always returns 0 to indicate successful execution.
+ */
+int main(int argc, char *argv[])
+{
+if(argc == 3)
+{
+printf("%d\n",atoi(argv[1]) * atoi(argv[2]));
+return (0);
+}
+else
+{
+printf("error\n");
+return (1);
+}
+}
