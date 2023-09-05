425f100e3cbbe078a3811deeb9143be3dd4f783b
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-05 14:20:24 -0700

    commit changes

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * argstostr - Brief description of the function.
+ * @ac : arument count
+ * @av : argument vector
+ * Detailed description of what the function does.
+ * Return: Description of the return value.
+ */
+char *argstostr(int ac, char **av)
+{
+int j,i,lent,k;
+char *argstor;
+i = 0;
+j = 0;
+lent = 0;
+k = 0;
+while (i <= ac)
+{
+while (av[j])
+{
+lent += j++;
+}
+j = 0;
+}
+if (ac == 0 || av == NULL)
+{
+return (NULL);
+}
+argstor = malloc(lent * sizeof(char *));
+if (argstor == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+{
+argstor[k] = av[i][j];
+k++;
+}
+}
+return (argstor);
+}
