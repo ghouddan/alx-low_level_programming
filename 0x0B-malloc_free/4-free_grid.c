7f8981d112ad9554a3e3f9efe4e5726aadc5a8c3
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-05 12:00:13 -0700

    commit changes

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,7 +2,7 @@
 #include "main.h"
 /**
  * free_grid - Brief description of the function.
- * @grid : the 2D array 
+ * @grid : the 2D array
  * @height : height of the matrix
  * Detailed description of what the function does.
  * Return: Description of the return value.
