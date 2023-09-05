6d3f3876b00a7ec21abde60918159063c929a807
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-05 11:43:00 -0700

    commit changes

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,23 +11,26 @@ int **alloc_grid(int width, int height)
 {
 int i, j;
 int **matrix;
-i = 0;
-j = 0;
-if(width <= 0 && height <= 0)
+if (width <= 0 || height <= 0)
 {
 return (0);
 }
-matrix = malloc(height * sizeof(int));
-if(matrix == 0)
+matrix = (int **)malloc(height * sizeof(int *));
+if (matrix == 0)
 {
 return (0);
 }
 for (i = 0; i < height; i++)
 {
-matrix[i] = malloc(width * sizeof(int));
-if(matrix[i] == 0)
+matrix[i] = (int *)malloc(width * sizeof(int));
+if (matrix[i] == 0)
 {
-return (0);
+for (j = 0; j < i; j++) {
+free(matrix[j]);
+}
+free(matrix);
+return NULL;
+
 }
 for (j = 0; j < width; j++)
 {
