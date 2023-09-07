4b503cb2dfa82ba6b5bd32c831a9eb96b5f9c5d3
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-07 13:21:10 -0700

    commit changes

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,21 +11,16 @@ int *array_range(int min, int max)
 {
     int *array;
     int i, size, j = 0;
-
     if (min > max)
         return (NULL);
-
     size = (max - min) + 1;
     array = malloc(size * sizeof(int));
-
     if (array == NULL)
         return (NULL);
-
     for (i = min; i <= max; i++)
     {
         array[j] = i;
         j++;
     }
-
     return (array);
 }
