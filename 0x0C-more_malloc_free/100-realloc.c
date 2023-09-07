cccad7da47e44ae98e645facbfacf0cb77939983
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-07 14:41:59 -0700

    commit changes

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,39 +2,40 @@
 #include <stdlib.h>
 
 /**
- * array_range - Creates an array of integers from min to max (inclusive).
- * @min: The minimum value.
- * @max: The maximum value.
- *
+ * _realloc - Creates an array of integers from min to max (inclusive).
+ * @ptr: data 
+ * @old_size: the olde size of the data
+ * @new_size : the new size of the data
  * Return: A pointer to the newly created array.
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-     void *new_ptr;
-     unsigned int i;
-     if (new_size == old_size)
-         return (ptr);
-
-     if (new_size == 0 && ptr != NULL)
-     {
-         free(ptr);
-         return (NULL);
-     }
-     new_ptr = malloc(new_size);
-     if (new_ptr == NULL)
-         return (NULL);
-     if (new_size > old_size)
-     {
-         for (i = 0; i < old_size; i++)
-     {
-                 *((char *)new_ptr + i) = *((char *)ptr + i);
-     }
-     }
-     if (new_size < old_size)
-     {
-         for (i = 0; i < new_size; i++)
-		 *((char *)new_ptr + i) = *((char *)ptr + i);
-     }
-     free(ptr);
-     return (new_ptr);
+void *new_ptr;
+unsigned int i;
+if (new_size == old_size)
+    return (ptr);
+if (new_size == 0 && ptr != NULL)
+{
+    free(ptr);
+    return (NULL);
+}
+if (!ptr)
+    return (malloc(new_size));
+new_ptr = malloc(new_size);
+if (new_ptr == NULL)
+    return (NULL);
+if (new_size > old_size)
+{
+    for (i = 0; i < old_size; i++)
+{
+  *((char *)new_ptr + i) = *((char *)ptr + i);
+}
+}
+if (new_size < old_size)
+{
+    for (i = 0; i < new_size; i++)
+  *((char *)new_ptr + i) = *((char *)ptr + i);
+}
+free(ptr);
+return (new_ptr);
 }
