705db29821e9b7e19e6dbb1d80a90f9a5aa0dbb9
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-07 14:30:20 -0700

    commit changes

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -23,12 +23,18 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
      new_ptr = malloc(new_size);
      if (new_ptr == NULL)
          return (NULL);
-     for (i = 0; i < old_size; i++)
+     if (new_size > old_size)
      {
-         *((char *)new_ptr + i) = *((char *)ptr + i);
+         for (i = 0; i < old_size; i++)
+     {
+                 *((char *)new_ptr + i) = *((char *)ptr + i);
+     }
+     }
+     if (new_size < old_size)
+     {
+         for (i = 0; i < new_size; i++)
+		 *((char *)new_ptr + i) = *((char *)ptr + i);
      }
      free(ptr);
      return (new_ptr);
-
-
 }
