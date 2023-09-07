dca9ec309735303f70f68b762620feb9da435f3c
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-07 03:02:57 -0700

    commit changes

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - Brief description of the function.
+ * @b : size of the memory
+ * Detailed description of what the function does.
+ * Return: Description of the return value.
+ */
+void *malloc_checked(unsigned int b)
+{
+void *p = malloc(b);
+if (p == 0)
+{
+exit (98);
+}
+return (p);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+this folder contain the task  0x0C-more_malloc_free
