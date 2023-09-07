a09631dc1d101343036c2e14429aec26cc868681
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-07 14:47:56 -0700

    commit changes

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,9 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * _realloc - Creates an array of integers from min to max (inclusive).
- * @ptr: data 
+ * _realloc - discription of the function.
+ * @ptr: data
  * @old_size: the olde size of the data
  * @new_size : the new size of the data
  * Return: A pointer to the newly created array.
@@ -13,28 +12,36 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 void *new_ptr;
 unsigned int i;
 if (new_size == old_size)
-    return (ptr);
+{
+return (ptr);
+}
 if (new_size == 0 && ptr != NULL)
 {
-    free(ptr);
-    return (NULL);
+free(ptr);
+return (NULL);
 }
-if (!ptr)
-    return (malloc(new_size));
+if (ptr == NULL)
+{
+return (malloc(new_size));
+}
 new_ptr = malloc(new_size);
 if (new_ptr == NULL)
-    return (NULL);
+{
+return (NULL);
+}
 if (new_size > old_size)
 {
-    for (i = 0; i < old_size; i++)
+for (i = 0; i < old_size; i++)
 {
-  *((char *)new_ptr + i) = *((char *)ptr + i);
+*((char *)new_ptr + i) = *((char *)ptr + i);
 }
 }
 if (new_size < old_size)
 {
-    for (i = 0; i < new_size; i++)
-  *((char *)new_ptr + i) = *((char *)ptr + i);
+for (i = 0; i < new_size; i++)
+{
+*((char *)new_ptr + i) = *((char *)ptr + i);
+}
 }
 free(ptr);
 return (new_ptr);
