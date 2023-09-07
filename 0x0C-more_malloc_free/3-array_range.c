b1e1bc40ba561b7e3a4fc72d0eeb88978efccd70
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-07 13:15:15 -0700

    commit changes

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * array_range - Brief description of the function.
  * @min : first number
@@ -10,18 +9,18 @@
  */
 int *array_range(int min, int max)
 {
-  int *array;
-  int i, size, j = 0;
-  if (min > max)
-    return (NULL);
-  size = (max - min) + 1;
-  array = malloc(size * sizeof(int));
-  if (array == NULL)
-    return (NULL);
-  for (i = min; i <= max; i++)
-  {
-    array[j] = i;
-    j++;
-  }
-  return (array);
+int *array;
+int i, size, j = 0;
+if (min > max)
+return (NULL);
+size = (max - min) + 1;
+array = malloc(size * sizeof(int));
+if (array == NULL)
+return (NULL);
+for (i = min; i <= max; i++)
+{
+array[j] = i;
+j++;
+}
+return (array);
 }
