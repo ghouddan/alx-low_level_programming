66fa69df0c5ed11a1514407fb5c90977101cfbfb
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-07 14:18:21 -0700

    commit changes

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - Creates an array of integers from min to max (inclusive).
+ * @min: The minimum value.
+ * @max: The maximum value.
+ *
+ * Return: A pointer to the newly created array.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+     void *new_ptr;
+     unsigned int i;
+     if (new_size == old_size)
+         return (ptr);
+
+     if (new_size == 0 && ptr != NULL)
+     {
+         free(ptr);
+         return (NULL);
+     }
+     new_ptr = malloc(new_size);
+     if (new_ptr == NULL)
+         return (NULL);
+     for (i = 0; i <= old_size; i++)
+     {
+         *((char *)new_ptr + i) = *((char *)ptr + i);
+     }
+     free(ptr);
+     return (new_ptr);
+
+
+}
