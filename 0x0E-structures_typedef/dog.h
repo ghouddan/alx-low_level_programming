cf8869b93874dd0ec161551afc9544f61f74a1c2
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-11 05:45:30 -0700

    add description

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog{
-	char *name;
-       	float age;
-       	char *owner;
+/**
+ * struct dog - a structure representing a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: This structure represents a dog, including its name, age, and owner's name.
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
