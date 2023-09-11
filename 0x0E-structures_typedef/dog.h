05db46a5784cb182e8241e0c93de177271d20348
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-11 14:28:19 -0700

    commit changes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,13 @@
  *
  * Description: This structure represents a dog, including its name, age, and owner's name.
  */
-typedef struct dog
+struct dog
 {
     char *name;
     float age;
     char *owner;
-} dog_t;
+};
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
