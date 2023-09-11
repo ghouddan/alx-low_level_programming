cbac3ae6505626f04bc7ff5015f064cf3d6ebc72
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-11 04:33:26 -0700

    commit tasck

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point of the program.
+ *
+ * bref description of the main function
+ *
+ * Return: Always returns 0 to indicate successful execution.
+ */
+int main()
+{
+printf("%s\n", __FILE__);
+return (0);
+}
