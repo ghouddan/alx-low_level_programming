abd97b4ccc240a0121ab6700fe9330916cd37163
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-11 05:30:52 -0700

    struct dog ; unit dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * init_dog - discription of the function.
+ * @d : struct
+ * @name : strunct.name
+ * @owner:struct.owner
+ * @age : struct.age
+ * Return: A pointer to the newly created array.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner){
+    if (d)
+    {    
+        d->name = name;
+        d->age = age;
+        d->owner = owner;
+    }
+}
