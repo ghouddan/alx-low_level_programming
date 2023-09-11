4fc2495c207194acc778d76d12dedfbef4e6658d
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-11 14:36:23 -0700

    commit changes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog - a structure representing a dog
  * @name: the name of the dog
@@ -10,20 +11,19 @@
  */
 struct dog
 {
-char *name;
-float age;
-char *owner;
+    char *name;
+    float age;
+    char *owner;
 };
-/**
- * dog_t - typedef for struct dog
- */
 
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcp(char *dest, char *src);
 int _strlen(char *string);
+
 #endif
 
