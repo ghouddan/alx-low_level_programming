7e6a403b4bff3f8c01c9f61f9badaf703101eb31
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-11 14:57:13 -0700

    commit changes

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,17 +2,17 @@
 /**
  * init_dog - discription of the function.
  * @d : struct
- * @name : strunct.name
- * @owner:struct.owner
- * @age : struct.age
+ * @name : strunct_name
+ * @age : struct_age
+ * @owner:struct_owner
  * Return: A pointer to the newly created array.
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 if (d)
 {
-d->name = name;
-d->age = age;
-d->owner = owner;
+d -> name = name;
+d -> age = age;
+d -> owner = owner;
 }
 }
