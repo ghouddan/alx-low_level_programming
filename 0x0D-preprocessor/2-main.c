f8cb47dec2eb3083ceb2f67880dd816953485c72
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-11 04:47:06 -0700

    fix error

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
 /**
  * main - Entry point of the program.
  * Return: Always returns 0 to indicate successful execution.
  */
-int main()
+int main(void)
 {
 printf("%s\n", __FILE__);
 return (0);
