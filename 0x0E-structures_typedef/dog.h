b38e2698e3c3fea46e0b85bc451b4ebb9239bc7e
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-11 05:52:09 -0700

    commit changes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
  * struct dog - a structure representing a dog
  * @name: the name of the dog
@@ -15,17 +14,6 @@ struct dog
     float age;
     char *owner;
 };
-
-/**
- * init_dog - initializes a dog structure
- * @d: pointer to the dog structure
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- *
- * Description: This function initializes a dog structure with provided name, age, and owner.
- */
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
 #endif
 
