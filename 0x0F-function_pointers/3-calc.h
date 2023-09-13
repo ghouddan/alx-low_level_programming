175537ce2cdaae82efe9ebee6abe56c8449f1a03
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-13 11:20:55 -0700

    commit changes

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -2,7 +2,6 @@
 #define CALC_H
 /**
  * struct op - Struct op
- *
  * @op: The operator
  * @f: The function associated
  */
@@ -10,7 +9,8 @@ typedef struct op
 {
 char *op;
 int (*f)(int a, int b);
-} op_t;
+};
+typedef struct op op_t;
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
