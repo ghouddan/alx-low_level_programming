7cc1f58e6a392f28d4ae87bec62dd3f699d85f01
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-13 11:14:17 -0700

    commit changes

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -4,21 +4,20 @@
 /**
  * get_op_func - Brief description of the function.
  * @s : oprator
- * @a : number 1
- * @b : number 2
  * Detailed description of what the function does.
  * Return: Description of the return value.
  */
 int (*get_op_func(char *s))(int, int)
 {
-op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
+op_t ops[] =
+{
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
 int i = 0;
 while (i < 5)
 {
