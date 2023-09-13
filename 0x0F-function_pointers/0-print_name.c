0b9d44e7bd021ab85fcba1acb163978e2cc78da4
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-13 06:42:23 -0700

    commit changes

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,16 @@
+#include <stdlib.h>
 #include "function_pointers.h"
 /**
  * _memcpy - Brief description of the function.
  * @name : string to print
- * @f : 
+ * @f : function pointer 
  * Detailed description of what the function does.
  * Return: Description of the return value.
  */
 void print_name(char *name, void (*f)(char *))
 {
+if ( name != NULL && f != NULL )
+{	
 f(name);
+}
 }
