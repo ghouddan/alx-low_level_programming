d7fb08f917d61ab324b1983c8775916b50f29a21
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-13 07:36:02 -0700

    commit changes

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,23 +10,17 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-int i, j = 0;
+int i;
 if (array != NULL && size <= 0 && cmp != NULL)
 {
 return (-1);
 }
 for (i = 0; i < size; i++)
 {
-if (cmp(array[i]) != 0)
+if (cmp(*(array + i)) != 0)
 {
 return (i);
-j++;
 }
 }
-if (j == 0)
-{
 return (-1);
-}
-
-return (0);
 }
