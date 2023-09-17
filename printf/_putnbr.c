7375f779d3edd833ca7b43b2999aa7a2a01b0916
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-17 04:19:55 -0700

    commit task 0123

---
+++ printf/_putnbr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdarg.h>
+/**
+ * print_all - Description of the function.
+ * @format : type of the varaible
+ * @...: Description of the variadic arguments.
+ *
+ * Return: Description of the return value (if applicable).
+ */
+void _putnbr(int n, int *lent)
+{
+	//unsigned int nb;
+	if (n < 0)
+	{
+		_putchar('-', lent);
+		n *= -1;
+	}
+	if (n < 10)
+		_putchar(n + '0', lent);
+	else
+	{
+		_putnbr(n / 10, lent);
+		_putnbr(n % 10, lent);
+	}
+}
---
+++ printf/compile/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - description
+ *
+ * Return : 0
+ */
+int main(void)
+{
+	char *c = "hello world!";
+	char a = 'A';
+	unsigned int numb = 32;
+	unsigned int n = 25346;
+	int b = 1432;
+	_printf("%X, %b, %s, %c, %d\n", n, numb, c, a, b);
+	return (0);
+}
