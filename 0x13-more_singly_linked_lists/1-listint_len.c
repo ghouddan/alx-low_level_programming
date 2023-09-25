c0a4d7966f7f0f79040d4fd09087c99571644beb
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-25 13:01:32 -0700

    function to print the number of element

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - Description of the function.
+ * @h : separetor between numbers
+ *
+ * Return: Description of the return value (if applicable).
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
