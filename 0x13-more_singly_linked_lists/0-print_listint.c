382a3bb34041b3bb3767b6079331d7d85b8ca2e6
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-25 12:49:19 -0700

    function to print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list - Description of the function.
+ * @h : separetor between numbers
+ *
+ * Return: Description of the return value (if applicable).
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
