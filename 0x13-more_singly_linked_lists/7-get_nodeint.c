970a67acfae7161a122e56a58e6093863cd140a9
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-09-26 13:27:59 -0700

    function to print the index of node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index- Description of the function.
+ * @head: head of the list
+ * @index : index of the node
+ *
+ * Return: Description of the return value (if applicable).
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+
+	unsigned int i = 0;
+	while (head)
+	{
+		if (i == index)
+			return (head);
+		i++;
+		head = head->next;
+	}
+	return (NULL);
+}
+
