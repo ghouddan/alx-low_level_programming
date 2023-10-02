e46833dc5fe313c3280668753fbbd04fbc18480e
mohammed ghouddan <ghouddanmohamed90@gmail.com>
2023-10-02 12:16:40 -0700

    function to creat a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * creat_file - Description of the function.
+ * @filename : name of file
+ * @text_content : content of file
+ * Return: Description of the return value (if applicable).
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	int lent;
+	ssize_t bytes_write;
+
+	if (filename == NULL)
+		return (-1);
+	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (file == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(file);
+		return (1);
+	}
+	for (lent = 0; text_content[lent]; lent++)
+		;
+	bytes_write = write(file, text_content, lent);
+	if (bytes_write == -1)
+	{
+		close(file);
+		return (-1);
+	}
+
+	close(file);
+	return (1);
+}
